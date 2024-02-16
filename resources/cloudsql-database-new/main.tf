1123540515f211e883b5fc87e037af447f2fe8bf
Nirmal-2204 <100931962+Nirmal-2204@users.noreply.github.com>
2024-02-16 17:17:26 +0530

    Update main.tf for creating new user and password every time with new database..

--- resources/cloudsql-database-new/main.tf
+++ resources/cloudsql-database-new/main.tf
@@ -5,6 +5,20 @@ resource "random_string" "cloudsql_database_name" {
   upper   = false
 }
 
+resource "random_string" "cloudsql_user_name" {
+  length  = 10
+  special = false
+  lower   = true
+  upper   = false
+}
+
+resource "random_string" "cloudsql_user_password" {
+  length  = 10
+  special = true
+  lower   = true
+  upper   = true
+}
+
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/sql_database_instance
 data "google_sql_database_instance" "instance" {
   name = var.instance_name
@@ -14,4 +28,11 @@ data "google_sql_database_instance" "instance" {
 resource "google_sql_database" "database" {
   name     = "database-${random_string.cloudsql_database_name.result}"
   instance = var.instance_name
+}
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_user
+resource "google_sql_user" "user" {
+  name     = random_string.cloudsql_user_name.result
+  instance = google_sql_database_instance.instance.name
+  password = random_string.cloudsql_user_password.result
 }
